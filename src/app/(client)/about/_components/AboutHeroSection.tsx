ad357f7f6b0a0fdd2304c892234600e80c7e0954
tushu003 <tahminata2015@gmail.com>
2025-11-25 14:20:55 +0600

    structerd folders

---
+++ src/app/(client)/about/_components/AboutHeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutHeroSection() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/app/(client)/about/_components/TeamSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TeamSection() {
+  return (
+    <div>TeamSection</div>
+  )
+}
